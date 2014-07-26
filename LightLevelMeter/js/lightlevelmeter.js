88a9fac45a6b3310aa197b78245f00f92b2a5f56
Kalpa Welivitigoda <callkalpa@gmail.com>
2014-07-26 15:52:39 +0530

    UI fixed

--- LightLevelMeter/js/lightlevelmeter.js
+++ LightLevelMeter/js/lightlevelmeter.js
@@ -1,46 +1,47 @@
-var max = -1;
-var min = 10000;
-var value = 0;
-var chartData = new TimeSeries();
-var smoothie = new SmoothieChart({yRangeFunction: myYRangeFunction});
+function init() {
+    var max = -1;
+    var min = 10000;
+    var value = 0;
+    var chartData = new TimeSeries();
+    var smoothie = new SmoothieChart({yRangeFunction: myYRangeFunction});
 
-smoothie.streamTo(document.getElementById("mycanvas"), 1000);
+    smoothie.streamTo(document.getElementById("mycanvas"), 1000);
 
-function myYRangeFunction(range) {
-    return {min: min, max: max};
-}
-
-function updateMax(max_value) {
-    document.getElementById("max").innerHTML = max_value;
-}
-
-function updateMin(min_value) {
-    document.getElementById("min").innerHTML = min_value;
-}
-
-document.getElementById('results').innerHTML = "waiting for change in light level...";
-
-window.addEventListener('devicelight', function (event) {
-    value = event.value;
-    document.getElementById("results").innerHTML = value;
-
-    if (value > max) {
-        max = value;
-        updateMax(max);
-        smoothie.maxValue = max;
+    function myYRangeFunction(range) {
+        return {min: min, max: max};
     }
 
-    if (value < min) {
-        min = value;
-        updateMin(min);
-        smoothie.minValue = min;
+    function updateMax(max_value) {
+        document.getElementById("max").innerHTML = max_value;
     }
-});
 
-smoothie.addTimeSeries(chartData, {fillStyle: 'rgb(255,0,0'});
+    function updateMin(min_value) {
+        document.getElementById("min").innerHTML = min_value;
+    }
 
-setInterval(function () {
-    chartData.append(new Date().getTime(), value);
-}, 1000);
+    document.getElementById('results').innerHTML = "waiting for change in light level...";
 
+    window.addEventListener('devicelight', function (event) {
+        value = event.value;
+        document.getElementById("results").innerHTML = value;
 
+        if (value > max) {
+            max = value;
+            updateMax(max);
+            smoothie.maxValue = max;
+        }
+
+        if (value < min) {
+            min = value;
+            updateMin(min);
+            smoothie.minValue = min;
+        }
+    });
+
+    smoothie.addTimeSeries(chartData, {fillStyle: 'rgb(255,0,0'});
+
+    setInterval(function () {
+        chartData.append(new Date().getTime(), value);
+    }, 1000);
+
+}
